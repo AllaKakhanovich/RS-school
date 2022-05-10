e388d0de13eb5f4d888c825567dd531120662b5a
Alla <allakakhanovich@gmail.com>
2022-05-10 14:42:36 +0300

    feat: removed tag from html

--- keyboard/src/app/app.js
+++ keyboard/src/app/app.js
@@ -1,21 +1,25 @@
-function myFunction() {
+function myCreateElement() {
   const area = document.createElement("textarea");
   const text = document.createTextNode("write here");
   area.appendChild(text);
   document.body.prepend(area);
   area.className = "field";
   
-  let div = document.createElement('div');
-  div.className = "message";
-  div.innerHTML = "часть keyboard (только цифры и англ. буквы) в системе window";
-  document.body.prepend(div);  
+  let p = document.createElement('p');
+  p.className = "message";
+  p.innerHTML = "ссылка на pull request https://github.com/AllaKakhanovich/RS-school/pull/1; часть keyboard (только цифры и англ. буквы) в системе window";
+  document.body.prepend(p); 
+  
+  const div = document.createElement('div');
+  document.body.append(div);
+  div.id = "keyboard";   
 }
 
-myFunction();
+myCreateElement();
 
 const keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61,
   113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97,
-  115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 13, 122, 120, 99, 
+  115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 
   118, 98, 110, 109, 44, 46, 47];
 
 init();
